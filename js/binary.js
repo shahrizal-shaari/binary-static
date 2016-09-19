dc83ca8100ef837dd647b90671ae7c88b6640e90
Shahrizal Shaari <shahrizal@binary.com>
2016-09-19 13:17:43 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -52916,10 +52916,12 @@ function format_money_jp(currency, amount) {
     var sign = '';
     var updatedAmount = amount;
     if(currency === 'JPY') { // remove decimal points and add comma.
+        console.log(updatedAmount + '  ' );
         if (Number(updatedAmount) < 0 ) {
            sign = '-';
         }
-
+        console.log('## ' + sign);
+   
         updatedAmount = updatedAmount.replace(/\.\d+$/, '').replace('-','').replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
